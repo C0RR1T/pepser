8b7ea98e6bc6a0d91ddb44a43c02d841627ccb96
C0RR1T <corsin@ragett.li>
2022-03-14 08:01:53 +0100

    new dir

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('contains the add pony button', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Add Pony/i);
-  expect(linkElement).toBeInTheDocument();
-});
