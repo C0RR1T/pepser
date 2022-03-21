c6a6efcdeef7833bbc57db49791ce4d1dc64dd90
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-03-21 11:55:21 +0100

    complete post
    no u corsin

--- spring-boot/src/main/java/ch/bbw/m226/openapiproject/MessageBoardService.java
+++ spring-boot/src/main/java/ch/bbw/m226/openapiproject/MessageBoardService.java
@@ -6,23 +6,18 @@ import ch.bbw.m226.openapi.generated.dto.Post;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class MessageBoardService {
 
-    private Map<Integer, Category> categories;
-    private Map<Integer, List<Post>> posts;
-    private Map<Integer, List<Comment>> comments;
+    private final Map<Integer, Category> categories = new HashMap<>();
+    private final Map<Integer, List<Post>> posts = new HashMap<>();
+    private final Map<Integer, List<Comment>> comments = new HashMap<>();
+
+    private final Random random = new Random();
 
     public MessageBoardService() {
-        this.categories = new HashMap<>();
-        this.posts = new HashMap<>();
-        this.comments = new HashMap<>();
-
         this.categories.put(1, new Category()
                 .id(1)
                 .description("Pictures of Rick Astley")
@@ -41,11 +36,45 @@ public class MessageBoardService {
         return this.categories.values().stream().toList();
     }
 
+    public Optional<Post> getPostById(Integer postId) {
+        return this.posts.values().stream()
+                .flatMap(Collection::stream)
+                .filter(post -> post.getId().equals(postId))
+                .findAny();
+    }
+
+    public Optional<List<Post>> getPosts(Integer categoryId) {
+        return Optional.of(this.posts.get(categoryId));
+    }
+
+    public Optional<Post> createPost(Post post) {
+        final var newId = random.nextInt();
+        final var createdDate = LocalDate.now();
+
+        post.id(newId).createdDate(createdDate);
+
+        return Optional.ofNullable(this.posts.get(post.getCategory())).map(posts -> {
+            posts.add(post);
+            this.comments.put(newId, new ArrayList<>());
+            return post;
+        });
+    }
+
     public Optional<List<Comment>> getComments(Integer postId) {
         return Optional.ofNullable(this.comments.get(postId));
     }
 
-    public Comment createComment(Integer postId, Comment comment) {
-        return null;
+    public Optional<Comment> createComment(Integer postId, Comment comment) {
+        final var commentId = this.random.nextInt();
+        final var createdDate = LocalDate.now();
+
+        comment.id(commentId).createdDate(createdDate);
+
+        return Optional.ofNullable(this.comments.get(postId)).map(comments -> {
+            comments.add(comment);
+            return comment;
+        });
     }
+
+
 }
--- spring-boot/src/main/java/ch/bbw/m226/openapiproject/PostController.java
+++ spring-boot/src/main/java/ch/bbw/m226/openapiproject/PostController.java
@@ -1,7 +1,6 @@
 package ch.bbw.m226.openapiproject;
 
 import ch.bbw.m226.openapi.generated.controller.PostsApi;
-import ch.bbw.m226.openapi.generated.dto.Category;
 import ch.bbw.m226.openapi.generated.dto.Comment;
 import ch.bbw.m226.openapi.generated.dto.Post;
 import org.springframework.http.HttpStatus;
@@ -9,70 +8,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.util.*;
+import java.util.List;
 
 @Validated
 @RestController
 public class PostController implements PostsApi {
 
-    private final Map<Integer, Category> categories = new HashMap<>();
-    private final Map<Integer, List<Post>> categoryPosts = new HashMap<>();
-    private final Map<Integer, Post> posts = new HashMap<>();
-    private final Map<Integer, Map<Integer, Comment>> comments = new HashMap<>();
-    private final Random random = new Random();
-
-    private MessageBoardService service;
+    private final MessageBoardService service;
 
     public PostController(MessageBoardService service) {
         this.service = service;
-
-        this.categories.put(1, new Category()
-                .id(1)
-                .description("Pictures of Rick Astley")
-                .name("Rick Astley Pics"));
-        this.posts.put(1, new Post().id(1)
-                .author("hugo")
-                .category(1)
-                .content("guten morgen")
-                .title("begrüssung")
-                .createdDate(LocalDate.of(2020, 5, 1))
-        );
-        this.categoryPosts.put(1, List.of(this.posts.get(1)));
     }
 
     @Override
     public ResponseEntity<Comment> createComment(Integer postId, Comment comment) {
-        final var postComments = this.comments.get(postId);
-
-        if (postComments == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        final var commentId = this.random.nextInt();
-        final var createdDate = LocalDate.now();
-
-        comment.id(commentId).createdDate(createdDate);
-
-        postComments.put(commentId, comment);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(comment);
+        return this.service.createComment(postId, comment)
+                .map(ResponseEntity.status(HttpStatus.CREATED)::body)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @Override
     public ResponseEntity<Post> createPost(Post post) {
-        final var newId = random.nextInt();
-        final var createdDate = LocalDate.now();
-
-        post.id(newId).createdDate(createdDate);
-
-        this.categoryPosts.putIfAbsent(post.getCategory(), new ArrayList<>());
-        this.categoryPosts.get(post.getCategory()).add(post);
-
-        this.posts.put(newId, post);
-        this.comments.put(newId, new HashMap<>());
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(post);
+        return this.service.createPost(post)
+                .map(ResponseEntity.status(HttpStatus.CREATED)::body)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @Override
@@ -83,13 +42,15 @@ public class PostController implements PostsApi {
     }
 
     @Override
-    public ResponseEntity<List<Post>> getPostById(Integer postId) {
-        return null;
+    public ResponseEntity<Post> getPostById(Integer postId) {
+        return this.service.getPostById(postId)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @Override
     public ResponseEntity<List<Post>> getPosts(Integer categoryId) {
-        return Optional.ofNullable(this.categoryPosts.get(categoryId))
+        return this.service.getPosts(categoryId)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
