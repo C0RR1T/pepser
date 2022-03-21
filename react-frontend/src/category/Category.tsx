74c569ed1a33966089a7da493bb847136476e641
Corsin Ragettli <corsin@ragett.li>
2022-03-21 23:00:55 +0100

    Category

--- react-frontend/src/category/Category.tsx
+++ react-frontend/src/category/Category.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { CategoryApi, PostApi } from '../generated/openapi';
 import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
+import { Card } from 'react-bootstrap/cjs';
+import { formatDate } from '../Post';
+import Button from 'react-bootstrap/Button';
 
 const Category = () => {
-    const {category: categoryId} = useParams();
-    const {data: categories} = useQuery('categories', () => new CategoryApi().)
-    const {data: posts, status} = useQuery(['posts', categoryId], () => new PostApi().getPosts({
+    const navigate = useNavigate();
+    const { category: categoryId } = useParams();
+    const { data: category } = useQuery(['category', categoryId], () => new CategoryApi().categoriesIdGet({
+        id: categoryId as unknown as number
+    }));
+    const { data: posts, status } = useQuery(['posts', categoryId], () => new PostApi().getPosts({
         categoryId: categoryId as unknown as number
-    }))
+    }));
     return (
         <Container>
-
+            <h1>{category?.name}</h1>
+            <p>{category?.description}</p>
+            <hr />
+            {
+                status === 'loading' && <Spinner animation={'border'} />
+            }
+            {
+                posts && posts.map(post => <Card key={post.id}>
+                    <Card.Body>
+                        <Card.Title>{post.title}</Card.Title>
+                        <Card.Subtitle>Created on {formatDate(post.createdDate)} by {post.author}</Card.Subtitle>
+                        <Card.Text>{post.content.substring(0, 25)}</Card.Text>
+                    </Card.Body>
+                    <Card.Footer><Button onClick={() => navigate(`/${categoryId}/${post.id}`)}>Go to
+                        post</Button></Card.Footer>
+                </Card>)
+            }
         </Container>
     );
 };
