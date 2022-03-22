cb719f093aefc2041b157cfde0fb8dded4f0b0f3
CORR1T <corsin@ragett.li>
2022-03-22 09:54:12 +0100

    get category by id

--- react-frontend/src/category/Category.tsx
+++ react-frontend/src/category/Category.tsx
@@ -11,31 +11,45 @@ import Button from 'react-bootstrap/Button';
 const Category = () => {
     const navigate = useNavigate();
     const { category: categoryId } = useParams();
-    const { data: category } = useQuery(['category', categoryId], () => new CategoryApi().categoriesIdGet({
-        id: categoryId as unknown as number
-    }));
-    const { data: posts, status } = useQuery(['posts', categoryId], () => new PostApi().getPosts({
-        categoryId: categoryId as unknown as number
-    }));
+    const { data: category } = useQuery(['category', categoryId], () =>
+        new CategoryApi().getCategoryById({
+            id: categoryId as unknown as number,
+        })
+    );
+    const { data: posts, status } = useQuery(['posts', categoryId], () =>
+        new PostApi().getPosts({
+            categoryId: categoryId as unknown as number,
+        })
+    );
     return (
         <Container>
             <h1>{category?.name}</h1>
             <p>{category?.description}</p>
             <hr />
-            {
-                status === 'loading' && <Spinner animation={'border'} />
-            }
-            {
-                posts && posts.map(post => <Card key={post.id}>
-                    <Card.Body>
-                        <Card.Title>{post.title}</Card.Title>
-                        <Card.Subtitle>Created on {formatDate(post.createdDate)} by {post.author}</Card.Subtitle>
-                        <Card.Text>{post.content.substring(0, 25)}</Card.Text>
-                    </Card.Body>
-                    <Card.Footer><Button onClick={() => navigate(`/${categoryId}/${post.id}`)}>Go to
-                        post</Button></Card.Footer>
-                </Card>)
-            }
+            {status === 'loading' && <Spinner animation={'border'} />}
+            {posts &&
+                posts.map(post => (
+                    <Card key={post.id}>
+                        <Card.Body>
+                            <Card.Title>{post.title}</Card.Title>
+                            <Card.Subtitle>
+                                Created on {formatDate(post.createdDate)} by{' '}
+                                {post.author}
+                            </Card.Subtitle>
+                            <Card.Text>
+                                {post.content.substring(0, 25)}
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <Button
+                                onClick={() =>
+                                    navigate(`/${categoryId}/${post.id}`)
+                                }>
+                                Go to post
+                            </Button>
+                        </Card.Footer>
+                    </Card>
+                ))}
         </Container>
     );
 };
